5a01102d761c6c5a554b25cfe4a5257d2a56c995
levisc8 <levisc8@gmail.com>
2017-12-05 11:50:45 +0100

    Update hypothesis figures

--- R_new/Defense_Talk_Slide_Figures.R
+++ R_new/Defense_Talk_Slide_Figures.R
@@ -143,32 +143,103 @@ lines(x, predict(negativeGLM, type = 'response'))
 
 # Linear LRR Simulation
 x <- runif(14, 0, 240)
-y <- 5 + -.025 * x + rnorm(14, 0, 1.5) # phylogeny only. adding more error for demonstration purposes
+y1 <- 5 + -.025 * x + rnorm(14, 0, 1.5) # phylogeny only. adding more error for demonstration purposes
+PhyloLM <- lm(y1 ~ x)
+PhyloPreds <- predict(PhyloLM, se.fit = TRUE,
+                      interval = 'confidence',
+                      level = 0.95)
 
-PhyloLM <- lm(y ~ x)
-plot(y ~ x, xlab = 'Novelty',
-     ylab = 'Effect Size of Competition',
-     axes = FALSE,
-     cex.lab = 1.5)
-box(lty = 1)
-abline(PhyloLM)
+forPlot <- data.frame(Novelty = rep(x, 3),
+                      ESCR = rep(y1, 3),
+                      Type = c(rep('Phylo', 14),
+                               rep('Poor-Phylo',14),
+                               rep('Good-Phylo',14)),
+                      UpCI = c(PhyloPreds$fit[ ,1] + PhyloPreds$se.fit,
+                               PhyloPreds$fit[ ,1] + 0.3 * PhyloPreds$se.fit, # less error for poorly conserved traits
+                               PhyloPreds$fit[ ,1] + 0.75 * PhyloPreds$se.fit), # slightly less error for well conserved traits
+                      LoCI = c(PhyloPreds$fit[ ,1] - PhyloPreds$se.fit,
+                               PhyloPreds$fit[ ,1] - 0.3 * PhyloPreds$se.fit,
+                               PhyloPreds$fit[ ,1] - 0.75 * PhyloPreds$se.fit),
+                      stringsAsFactors = FALSE)
 
-y <- 5 + -0.025 * x + rnorm(14, 0, .5) # poorly conserved have smaller error
-poorLM <- lm(y ~ x)
-plot(y ~ x, xlab = 'Novelty',
-     ylab = 'Effect Size of Competition',
-     axes = FALSE,
-     cex.lab = 1.5)
-box(lty = 1)
-abline(poorLM)
+plt.blank <-  theme(panel.grid.major = element_blank(),
+                    panel.grid.minor = element_blank(),
+                    panel.background = element_blank(),
+                    panel.border = element_rect(size = 2,
+                                                fill = NA),
+                    axis.line = element_line(colour = "black"),
+                    axis.title = element_text(size = 20))
 
-y <- 5 + -.025 * x + rnorm(14, 0, 1) # well conserved traits have medium amount of error
-goodLM <- lm(y ~ x)
-plot(y ~ x, xlab = 'Novelty',
-     ylab = 'Effect Size of Competition',
-     axes = FALSE,
-     cex.lab = 1.5)
-box(lty = 1)
-abline(goodLM)
+Plt1 <- ggplot(data = filter(forPlot, Type == 'Phylo'),
+               aes(x = Novelty, y = ESCR)) + 
+  plt.blank +
+  geom_ribbon(aes(ymin = LoCI, 
+                  ymax = UpCI),
+              fill = 'red',
+              alpha = 0.6) +
+  geom_abline(aes(slope = coef(PhyloLM)[2],
+                  intercept = coef(PhyloLM)[1])) +
+  scale_y_continuous('Effect Size of Competition',
+                     breaks = NULL,
+                     labels = NULL) +
+  scale_x_continuous('Novelty',
+                     breaks = NULL,
+                     labels = NULL)
 
+Plt1
 
+Plt2 <- ggplot(data = filter(forPlot, Type == 'Good-Phylo'),
+               aes(x = Novelty, y = ESCR)) + 
+  plt.blank +
+  geom_ribbon(aes(ymin = LoCI, 
+                  ymax = UpCI),
+              fill = 'grey40',
+              alpha = 0.6) +
+  geom_ribbon(data = filter(forPlot, Type == 'Phylo'),
+              aes(ymin = LoCI,
+                  ymax = UpCI),
+              fill = 'red',
+              alpha = 0.2) +
+  geom_abline(aes(slope = coef(PhyloLM)[2],
+                  intercept = coef(PhyloLM)[1])) +
+  scale_y_continuous('Effect Size of Competition',
+                     breaks = NULL,
+                     labels = NULL) +
+  scale_x_continuous('Novelty',
+                     breaks = NULL,
+                     labels = NULL)
+Plt2
+
+Plt3 <- ggplot(data = filter(forPlot, Type == 'Poor-Phylo'),
+               aes(x = Novelty, y = ESCR)) + 
+  plt.blank +
+  geom_ribbon(aes(ymin = LoCI,
+                  ymax = UpCI),
+              fill = 'black',
+              alpha = 0.8) + 
+  geom_ribbon(data = filter(forPlot, Type == 'Good-Phylo'),
+              aes(ymin = LoCI, 
+                  ymax = UpCI),
+              fill = 'grey40',
+              alpha = 0.5) +
+  geom_ribbon(data = filter(forPlot, Type == 'Phylo'),
+              aes(ymin = LoCI,
+                  ymax = UpCI),
+              fill = 'red',
+              alpha = 0.25) +
+  geom_abline(aes(slope = coef(PhyloLM)[2],
+                  intercept = coef(PhyloLM)[1])) +
+  scale_y_continuous('Effect Size of Competition',
+                     breaks = NULL,
+                     labels = NULL) +
+  scale_x_continuous('Novelty',
+                     breaks = NULL,
+                     labels = NULL)
+Plt3
+
+phylo <- tyson$phylo
+kstra <- filter(communities, exotic_species == 'Kummerowia_striata')
+
+kstraPhylo <- drop.tip(phylo, setdiff(phylo$tip.label, kstra$community))
+
+plot(kstraPhylo, show.tip.label = FALSE)
